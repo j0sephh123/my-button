d0c77bde30a52734237f0e664d4d81e522a60e40
Joseph <iossif123gg@gmail.com>
2024-09-10 08:53:24 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: "./src/Button.tsx",
+      name: "Button",
+      fileName: (format) => `button.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
